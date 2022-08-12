cd442918e31da61c506b7bd5d5196d3a801d6d40
whacko23 <Whacko23@users.noreply.github.com>
2022-08-11 19:08:37 -0500

    Initialized the project

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Rajanish Karki on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
